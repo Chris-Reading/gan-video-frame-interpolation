d7604c99a7a961879feeb1c63db1e18102eb14ec
Chris-Reading <122531090+Chris-Reading@users.noreply.github.com>
2024-04-16 00:45:08 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,10 +3,9 @@ from torchvision import transforms
 import numpy as np
 import os
 from PIL import Image
-import lightning as L
 
 worker_threads = 2
-batch_size = 32
+batch_size = 1
 class VimeoDataset(Dataset):
     def __init__(self, root_dir, split='train', transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5), (0.5))])):
         self.root_dir = root_dir
@@ -55,36 +54,37 @@ class VimeoDataset(Dataset):
 
         return frames, self.file_paths[idx]
     
-class VimeoDataModule(L.LightningDataModule):
-    def __init__(self, data_dir: str = 'src\\vimeo_septuplet', batch_size: int = batch_size, transform=transforms.ToTensor()):
-        super().__init__()
-        self.data_dir = data_dir
-        self.batch_size = batch_size
+class MSUDataset(Dataset):
+    def __init__(self, root_dir, transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5), (0.5))])):
+        self.root_dir = root_dir
         self.transform = transform
 
-    def prepare_data(self):
-        '''
-        Empty method since the dataset is stored locally not downloaded
-        More details here: https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html
-        '''
-        pass
+        # Load file paths for the specified split
+        split_file = os.path.join(root_dir, f"train.txt")
+        with open(split_file, 'r') as f:
+            self.file_paths = [line.strip() for line in f.readlines()]
 
-    def setup(self, stage: str):
-        if stage == 'fit' or stage is None: # Training dataset
-            train_set_full = VimeoDataset(root_dir=self.data_dir, split='train')
-            total_samples = len(train_set_full)
-            train_size = int(0.9 * total_samples)
-            val_size = total_samples - train_size
-            self.train, self.validate = random_split(train_set_full, [train_size, val_size])
-        
-        if stage == 'test' or stage is None:
-            self.test = VimeoDataset(root_dir=self.data_dir, split='test')
+    def __len__(self):
+        return len(self.file_paths)
 
-    def train_dataloader(self):
-        return DataLoader(self.train, batch_size=batch_size, num_workers=worker_threads)
+    def __getitem__(self, idx):
+        sequence_path = os.path.join(self.root_dir, self.file_paths[idx])
+        frames = []
+        paths = os.listdir(sequence_path)
+        for i in range(len(paths)):
+            frames.append(Image.open(os.path.join(sequence_path, paths[i])))
+
+        width, height = 1920, 1080
+        left_pad = (2048 - width) // 2
+        right_pad = 2048 - width - left_pad
+        top_pad = (2048 - height) // 2
+        bottom_pad = 2048 - height - top_pad
+        for i in range(len(frames)):
+            frames[i] = transforms.functional.pad(frames[i], (left_pad, top_pad, right_pad, bottom_pad))
+            frames[i] = (np.array(frames[i]).astype(np.float32) - 127.5) / 127.5
+
+            if self.transform:
+                frames[i] = self.transform(frames[i])
+
+        return frames, self.file_paths[idx]
     
-    def val_dataloader(self):
-        return DataLoader(self.validate, batch_size=batch_size, num_workers=worker_threads)
-
-    def test_dataloader(self):
-        return DataLoader(self.test, batch_size=batch_size, num_workers=worker_threads)
